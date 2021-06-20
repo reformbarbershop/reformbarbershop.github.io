ce8c627fdab6d69ba841a54bb1eb878a41ad6db4
Reform <BarbershopReform@gmail.com>
2021-06-20 13:23:33 +0300

    Removed files

--- components/Page/PageFooter.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer class="footer">
-    <div class="container">
-      <p>&copy; 2021 Reform Barbershop. Всі права захищено.</p>
-    </div>
-  </footer>
-</template>
--- plugins/element-ui.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import ElementUI from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/en'
-import '@/node_modules/element-ui/lib/theme-chalk/index.css'
-
-Vue.use(ElementUI, { locale })
