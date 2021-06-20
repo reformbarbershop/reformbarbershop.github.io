d6360a55b428be5b6cf6a3a9e5a0837b807f3d5c
Reform <BarbershopReform@gmail.com>
2021-06-20 13:11:51 +0300

    Generate files

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -12,7 +12,7 @@ Vue.use(Router)
 
 export const routerOptions = {
   mode: 'history',
-  base: '/',
+  base: '/reformbarbershop.github.io/',
   linkActiveClass: 'nuxt-link-active',
   linkExactActiveClass: 'nuxt-link-exact-active',
   scrollBehavior,
